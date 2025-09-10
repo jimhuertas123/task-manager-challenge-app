952abf5dd9f79c96272812b54b37d79461ae3eea
jimhuertas123 <jhuertas@unsa.edu.pe>
2025-09-10 11:25:30 -0500

    FEAT: header search (no functionality yet)

--- src/components/layout/MainLayout/MainLayout.tsx
+++ src/components/layout/MainLayout/MainLayout.tsx
@@ -1,14 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import { NavBar } from '../NavBar';
+import { SearchTasks } from '../SearchTasks';
 
 export const MainLayout = () => {
   return (
-    <div className="grid min-h-screen grid-cols-[232px_1fr] gap-x-8 bg-neutro-5 text-neutro pt-8 pl-8 pb-8">
+    <div className="grid min-h-screen grid-cols-[232px_1fr] gap-x-8 bg-neutro-5 text-neutro pt-8 pl-8 pb-8 pr-9">
       <NavBar />
       <main className="grid grid-rows-[64px_1fr] h-full gap-y-6 text-neutro-1">
-        <header className="bg-neutro-4 rounded-[24px] text-neutro-2 flex items-center px-6">
-          xDD
-        </header>
+        <SearchTasks />
         <Outlet />
       </main>
     </div>
